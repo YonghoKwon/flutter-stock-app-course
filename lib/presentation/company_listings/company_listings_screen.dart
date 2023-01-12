f0190ec2ddce5eea5a62f3a36f5f9f3f4edf3d55
YonghoKwon <ho2708@naver.com>
2023-01-12 23:29:30 +0900

    feature(강의) : ViewModel 연동

--- lib/presentation/company_listings/company_listings_screen.dart
+++ lib/presentation/company_listings/company_listings_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:stock_app/presentation/company_listings/company_listings_action.dart';
 import 'package:stock_app/presentation/company_listings/company_listings_view_model.dart';
 
 class CompanyListingsScreen extends StatelessWidget {
@@ -16,20 +17,24 @@ class CompanyListingsScreen extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.all(16.0),
               child: TextField(
+                onChanged: (query) {
+                  // 검색
+                  viewModel.onAction(CompanyListingsAction.onSearchQueryChange(query));
+                },
                 decoration: InputDecoration(
                   focusedBorder: OutlineInputBorder(
                     borderSide: BorderSide(
                       color: Theme.of(context).colorScheme.primary,
                       width: 2.0,
                     ),
-                    borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                    borderRadius: const BorderRadius.all(Radius.circular(5.0)),
                   ),
                   enabledBorder: OutlineInputBorder(
                     borderSide: BorderSide(
                       color: Theme.of(context).colorScheme.primary,
                       width: 2.0,
                     ),
-                    borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                    borderRadius: const BorderRadius.all(Radius.circular(5.0)),
                   ),
                   labelText: '검색...',
                   labelStyle: TextStyle(
@@ -40,7 +45,10 @@ class CompanyListingsScreen extends StatelessWidget {
             ),
             Expanded(
               child: RefreshIndicator(
-                onRefresh: () async {},
+                onRefresh: () async {
+                  // 새로고침
+                  viewModel.onAction(const CompanyListingsAction.refresh());
+                },
                 child: ListView.builder(
                   itemCount: state.compaines.length,
                   itemBuilder: (context, index) {
