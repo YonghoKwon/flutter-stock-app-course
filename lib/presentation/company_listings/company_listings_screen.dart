eeb9e56ae2200a3cb0cdceeaf49682adb1989cd1
YonghoKwon <ho2708@naver.com>
2023-01-15 00:01:44 +0900

    feature(강의) : 회사 정보 ViewModel & 회사 정보 화면 UI와 ViewModel 연동

--- lib/presentation/company_listings/company_listings_screen.dart
+++ lib/presentation/company_listings/company_listings_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'package:provider/provider.dart';
+import 'package:stock_app/domain/repository/stock_repository.dart';
+import 'package:stock_app/presentation/company_info/company_info_screen.dart';
+import 'package:stock_app/presentation/company_info/company_info_view_model.dart';
 import 'package:stock_app/presentation/company_listings/company_listings_action.dart';
 import 'package:stock_app/presentation/company_listings/company_listings_view_model.dart';
 
@@ -56,6 +60,22 @@ class CompanyListingsScreen extends StatelessWidget {
                       children: [
                         ListTile(
                           title: Text(state.compaines[index].name),
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) {
+                                  final repository = GetIt.instance<StockRepository>();
+                                  final symbol = state.compaines[index].symbol;
+                                  return ChangeNotifierProvider(
+                                    create: (_) => CompanyInfoViewModel(
+                                        repository, symbol),
+                                    child: const CompanyInfoScreen(),
+                                  );
+                                },
+                              ),
+                            );
+                          },
                         ),
                         Divider(
                           color: Theme.of(context).colorScheme.secondary,
